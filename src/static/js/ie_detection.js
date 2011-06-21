12dc89f542497cb8615594f3dd1a07df267541e8
Tobias von Klipstein <tobias@uxebu.com>
2011-06-22 01:51:22 +0200

    adding classnames for ie to the html element

---
+++ src/static/js/ie_detection.js
@@ -0,0 +1,10 @@
+(function(){
+	var v = 3,
+		div = document.createElement('div'),
+		all = div.getElementsByTagName('i');
+	while (
+		div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->',
+		all[0]
+	);
+	document.documentElement.className += v > 4 ? " ie" + v : ""; // modernized: thx to john-naked dalton :)
+}());
