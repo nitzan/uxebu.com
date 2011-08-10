409cbd2466845e2fd2be5613bd8551538691360b
Nikolai Onken <nikolai@nikolaionken.com>
2011-08-10 21:39:45 +0200

    Added meta tags for mobile

--- lib/blog/themes/Uxebu/header.php
+++ lib/blog/themes/Uxebu/header.php
@@ -2,6 +2,8 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="chrome=1">
 
 <title><?php bloginfo('name'); ?> <?php if ( is_single() ) { ?> &raquo; Blog Archiv <?php } ?> <?php wp_title(); ?></title>
 <link rel="stylesheet" href="/static/css/style.css" type="text/css" media="all" />
